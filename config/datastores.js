d23e8797481bb2325a4a18e4a151cf28d8a804c8
asdf1899 <araid.anas99@gmail.com>
2020-04-21 22:05:35 +0200

    Add model grado

--- config/datastores.js
+++ config/datastores.js
@@ -49,7 +49,7 @@ module.exports.datastores = {
     *                                                                          *
     ***************************************************************************/
     adapter: 'sails-mysql',
-    url: 'mysql://root@127.0.0.1:3306/my_sails',
+    url: 'mysql://root@127.0.0.1:3306/my_vvfire',
 
   },
 
