10b197577ee9d5f6bef72811560f80b4a1add7a0
asdf1899 <araid.anas99@gmail.com>
2020-08-12 18:00:22 +0200

    Vigile sorted by surname

--- api/controllers/vigile/show.js
+++ api/controllers/vigile/show.js
@@ -22,7 +22,7 @@ module.exports = {
   fn: async function (inputs) {
     let vigili = await Vigile.find({
       where: {fkCorpovvf: inputs.idcorpo}
-    });
+    }).sort('surname ASC');
     if (vigili === null || vigili === undefined || vigili === false){
       return [{error: '404', message: 'Not Found'}];
     }
