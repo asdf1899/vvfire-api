53fcaf2461e62565064f5cb8da0a77a4f052bfb3
asdf1899 <araid.anas99@gmail.com>
2020-09-04 19:22:10 +0200

    Fix corpovvf phone number bug

--- api/controllers/corpovvf/update.js
+++ api/controllers/corpovvf/update.js
@@ -17,7 +17,7 @@ module.exports = {
       allowNull: false
     },
     phone:{
-      type: 'number',
+      type: 'string',
       required: true,
       unique: true,
       allowNull: false
--- api/models/Corpovvf.js
+++ api/models/Corpovvf.js
@@ -14,7 +14,7 @@ module.exports = {
       allowNull: false
     },
     phone:{
-      type: 'number',
+      type: 'string',
       required: true,
       unique: true,
       allowNull: false
